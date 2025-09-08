f93cd7c70ed47ca3f3df15b18e1951ec1aa2127a
Vudattu Praneethnadh <142204604+vpraneethnadh@users.noreply.github.com>
2025-09-08 12:31:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import wasm from 'vite-plugin-wasm'
-import topLevelAwait from 'vite-plugin-top-level-await'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm(), topLevelAwait()],
-  base: '/',
-  optimizeDeps: {
-    exclude: ["sql.js/react"],
-    esbuildOptions: {
-      target: "es2020"
-    }
-  },
-  build: {
-    target: 'esnext'
-  }
-});
+  plugins: [react()],
+})
